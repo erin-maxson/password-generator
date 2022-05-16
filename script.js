df8de7a844362ffb2e1a491bf368952cf47fb77e
erin-maxson <erin@cohatch.com>
2022-05-16 15:49:25 -0700

    adding other confirmations

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ function writePassword() {
 
 function generatePassword() {
   var size = prompt("Please enter a number between 8 to 128 for your password characters.")
-console.log(size)
+  console.log(size)
 }
 
 if (size >= 8 && size <= 128) {
   var isLower = confirm ("Include lowercase characters?")
   console.log(isLower)
+
+  var isUpper = confirm ("Include uppercase characters?")
+  console.log(isUpper)
+
+  var isNumber = confirm ("Include numbers?")
+  console.log(isNumber)
+
+  var isSpecial = confirm ("Include special characters?")
+  console.log(isSpecial)
 }
 
 else {
