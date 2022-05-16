8912c61aeafdf51d9b03452baf6a9a5fd3862a81
erin-maxson <erin@cohatch.com>
2022-05-16 15:46:02 -0700

    adding variables for prompt, size/lowercase

--- script.js
+++ script.js
@@ -10,5 +10,20 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var size = prompt("Please enter a number between 8 to 128 for your password characters.")
+console.log(size)
+}
+
+if (size >= 8 && size <= 128) {
+  var isLower = confirm ("Include lowercase characters?")
+  console.log(isLower)
+}
+
+else {
+  alert("Invalid Entry")
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
